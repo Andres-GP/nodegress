8b2676f88a800c5dd271e42c57bfcafc772efc9b
Andres-GP <andresgarciapedreros@gmail.com>
2023-01-05 11:57:51 -0500

    user manipulation functionallities done

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: 'localhost',
-  user: 'postgres',
+  user: 'luyandy',
   password: '',
   database: 'firstapi',
   port: '5432'
@@ -10,10 +10,51 @@ const pool = new Pool({
 
 const getUsers = async (req, res) => {
   const response = await pool.query('SELECT * FROM users');
-  console.log(response.rows);
-  res.send('users');
+  res.status(200).json(response.rows);
+}
+
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+  const response = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+  res.json(response.rows)
+}
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+
+  await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+  console.log(response)
+  res.json({
+    message: 'User Added Succesfully',
+    body: {
+      user: { name, email }
+    }
+  })
+};
+
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { name, email } = req.body;
+  const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+    name,
+    email,
+    id
+  ]);
+  console.log(response)
+  res.json('User Updated succesfully')
+}
+
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const response = await pool.query('DELETE FROM users WHERE id = $1', [id])
+  console.log(response);
+  res.json(`User ${id} deleted succesfully`)
 }
 
 module.exports = {
-  getUsers
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
 }
